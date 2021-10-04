5cbe9c7b3c2ceb811ce1dc2b193c2446e75b7ef1
DawDwec <daw.dwec@gmail.com>
2021-10-04 12:59:35 +0200

    @ version 1.1 - añadimos el calculo de la mediana

--- AE1_JavaScript.js
+++ AE1_JavaScript.js
@@ -11,6 +11,9 @@
 //Definimos el array
 let array1 = [0, 88, 34, 67, 12, 90, 23, 34, 49 ,44];
 
+
+
+
 //Calculamos la suma de todos los elementos del array
 let suma = array1.reduce((previous, current) => current += previous);
 
@@ -21,8 +24,31 @@ let avg = suma / array1.length;
 console.log("El array contiene los siguientes elementos: " + array1);
 console.log("La media aritmetica del array es: " + avg);
 
+
+
+
 //Calculamos la mediana del array
-let medio = array1.length / 2;
-console.log(medio);
-let mediana = array1.slice(medio);
-console.log(mediana);
+if (array1.length%2 == 0) {
+    pos1 = array1.length/2;
+    pos2 = pos1 - 1;
+    mediana = (pos1 - pos2) / 2;
+} else {
+    pos_central = (array1.length - 1) / 2;
+    mediana = pos_central;
+}
+
+//Imprimimos en consola la mediana del array
+console.log("La mediana del array es: " + mediana);
+
+
+
+
+//Calculamos la moda del array
+
+//Ordenamos el array
+array1.sort();
+console.log("El array ordenado: " + array1);
+
+
+//Imprimimos la moda del array
+console.log("La moda del array es: " + moda);
